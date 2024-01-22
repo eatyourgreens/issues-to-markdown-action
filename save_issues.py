73b2212be200b6d0fb772aa6940236e9bff025d1
Jim O'Donnell <jim@zooniverse.org>
2024-01-22 21:13:51 +0000

    Rewrite path stuff

--- save_issues.py
+++ save_issues.py
@@ -1,12 +1,13 @@
 import os
 import re
 import requests
-from pathlib import Path, PurePath
+from pathlib import Path
 
 # create the default image directory, if it doesn't exist
-image_root = Path('images').mkdir(parents=True, exist_ok=True)
-# edit this if you want to save the markdown files to a subdirectory
-markdown_root = Path('./').mkdir(parents=True, exist_ok=True)
+image_root = Path('images')
+image_root.mkdir(parents=True, exist_ok=True)
+# change this if you want to save the markdown files to a subdirectory
+markdown_root = Path.cwd()
 
 def get_issues(repo, token, label):
     url = f"https://api.github.com/repos/{repo}/issues"
@@ -27,12 +28,11 @@ def download_and_save_image(url, issue_number):
     try:
         response = requests.get(url)
         if response.status_code == 200:
-            image_name = PurePath(url).name
+            image_name = Path(url).name
             image_folder = image_root / f'issue-{issue_number}'
             image_folder.mkdir(parents=True, exist_ok=True)
             path = image_folder / image_name
-            with open(path, 'wb') as file:
-                file.write(response.content)
+            path.write_bytes(response.content)
             return path
         else:
             print(f"Failed to download image: {url}")
@@ -43,19 +43,18 @@ def download_and_save_image(url, issue_number):
 
 def save_issue(issue):
     try:
-        title = issue.get('title', 'Untitled').replace(' ', '_')
         issue_number = issue.get('number', 'Unknown')
         issue_title = issue.get('title', 'Untitled')
-        md_file = markdown_root / f"{issue_number}_{title}.md"
-        with open(md_file, 'w') as file:
-            file.write(f"# {issue_title}\n\n")
-            body = issue.get('body', '')
-            image_urls = extract_image_urls(body)
-            for url in image_urls:
-                image_path = download_and_save_image(url, issue_number)
-                if image_path:
-                    body = body.replace(url, image_path)
-            file.write(body)
+        issue_body = issue.get('body', '')
+        md_filename = f"{issue_number}_{issue_title}.md".replace(' ', '_')
+        md_file = markdown_root / md_filename
+        md_file.write_text(f"# {issue_title}\n\n")
+        image_urls = extract_image_urls(issue_body)
+        for url in image_urls:
+            image_path = download_and_save_image(url, issue_number)
+            if image_path:
+                issue_body = issue_body.replace(url, image_path)
+        md_file.write_text(issue_body)
     except Exception as e:
         print(f"Error processing issue: {e}")
 
